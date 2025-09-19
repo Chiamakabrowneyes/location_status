ad80dd88d90475d506b00bbcf0b830e4d1d697c4
chiamakabrowneyes <chiamaka@uni.minerva.edu>
2025-09-19 11:45:56 -0700

    Fixing the point coverage

--- status.py
+++ status.py
@@ -1,13 +1,12 @@
-from shapely.geometry import Point, MultiPoint
 from constants import *
 import requests
-
+from shapely.geometry import Point, shape
 
 """Returns the parameter bounds and checks if the point is within bounds"""
-def in_bounds_shapely(points, p):
-    hull = MultiPoint(points).convex_hull
-    return hull.covers(Point(p))
-
+def in_bounds_geojson(zone, point):
+    poly = shape(zone)
+    lon, lat = point
+    return poly.covers(Point(lon, lat))
 
 """Returns the points and polygon for each clinician. """
 def get_clinician_status(clinician_id):
@@ -16,14 +15,15 @@ def get_clinician_status(clinician_id):
         return None
     point = None
     zone = None
+
     for feature in data['features']:
         if feature['geometry']['type'] == 'Point':
             point = feature['geometry']['coordinates']
         elif feature['geometry']['type'] in {"Polygon", "MultiPolygon"}:
-            zone = feature['geometry']['coordinates'][0]
+            zone = feature['geometry']
     if not point or not zone:
         return None
-    return in_bounds_shapely(zone, point)
+    return in_bounds_geojson(zone, point)
 
 
 """Returns status for all clinicians"""
